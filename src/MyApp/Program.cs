d085496e7dbcb36c45f72dd40e36a1568e79ac56
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2017-03-21 11:20:26 +0100

    init

---
+++ src/MyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyLib;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            MyHelper.DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/MyLib/MyHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class MyHelper
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("Doing something in MyLib!");
+        }
+    }
+}
